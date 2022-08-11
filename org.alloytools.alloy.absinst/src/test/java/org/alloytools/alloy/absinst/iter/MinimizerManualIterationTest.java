7848fbddc2cc32145eb9d7cacedb6c1eca06f924
Jan Oliver Ringert <jan.ringert@uni-weimar.de>
2022-08-11 14:51:25 +0200

    added test that does manual iteration of solutions and then minimizes each to count distinct abstract instances

---
+++ org.alloytools.alloy.absinst/src/test/java/org/alloytools/alloy/absinst/iter/MinimizerManualIterationTest.java
@@ -0,0 +1,75 @@
+package org.alloytools.alloy.absinst.iter;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.alloytools.alloy.absinst.Minimizer;
+import org.alloytools.alloy.absinst.UBKind;
+import org.junit.Test;
+
+import edu.mit.csail.sdg.alloy4.A4Reporter;
+import edu.mit.csail.sdg.ast.Command;
+import edu.mit.csail.sdg.parser.CompModule;
+import edu.mit.csail.sdg.parser.CompUtil;
+import edu.mit.csail.sdg.translator.A4Options;
+import edu.mit.csail.sdg.translator.A4Solution;
+import edu.mit.csail.sdg.translator.TranslateAlloyToKodkod;
+
+
+public class MinimizerManualIterationTest {
+
+    private static int instanceNum;
+    private static int abstractInstanceNum;
+
+    @Test
+    public void testSomeARun() {
+        String module = "src/test/alloy/basic/someA.als";
+        countInstances(module);
+        assertEquals(3, instanceNum);
+        assertEquals(1, abstractInstanceNum);
+    }
+
+    @Test
+    public void testAddressBook3a() {
+        String module = "../org.alloytools.alloy.extra/extra/models/book/chapter2/addressBook3a.als";
+
+        countInstances(module);
+        assertEquals(2647, instanceNum);
+        assertEquals(3, abstractInstanceNum);
+    }
+
+    private static void countInstances(String module) {
+        instanceNum = 0;
+        abstractInstanceNum = 0;
+        int cmdNum = 0;
+
+        List<A4Solution> instances = new ArrayList<>();
+
+        CompModule world = CompUtil.parseEverything_fromFile(A4Reporter.NOP, null, module);
+        Command command = world.getAllCommands().get(cmdNum);
+        A4Options options = new A4Options();
+        options.solver = A4Options.SatSolver.SAT4J;
+
+        A4Solution ans = TranslateAlloyToKodkod.execute_command(A4Reporter.NOP, world.getAllReachableSigs(), command, options);
+
+        while (ans.satisfiable()) {
+            instances.add(ans);
+            ans = ans.next();
+        }
+
+        instanceNum = instances.size();
+
+        Set<String> abstractInstances = new HashSet<>();
+        for (A4Solution inst : instances) {
+            Minimizer m = new Minimizer();
+            m.minimize(world, command, inst, options, UBKind.EXACT);
+            String absInst = m.getLowerBound().toString() + m.printUpperBound();
+            abstractInstances.add(absInst);
+        }
+        abstractInstanceNum = abstractInstances.size();
+    }
+}
