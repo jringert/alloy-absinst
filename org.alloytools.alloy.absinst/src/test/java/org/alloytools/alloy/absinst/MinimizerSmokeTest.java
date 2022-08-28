0befe793a3651642510e4aa8fdf597cc7e326209
jringert <jan_oliver.ringert@kcl.ac.uk>
2022-08-28 23:16:37 +0100

    added more specs to smoke test (issues with SubsetSigs and Enums)

--- org.alloytools.alloy.absinst/src/test/java/org/alloytools/alloy/absinst/MinimizerSmokeTest.java
+++ org.alloytools.alloy.absinst/src/test/java/org/alloytools/alloy/absinst/MinimizerSmokeTest.java
@@ -11,7 +11,10 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import edu.mit.csail.sdg.translator.A4Options.SatSolver;
 
 /**
  * Test that runs through all models just to see whether sanity checks pass
@@ -19,14 +22,34 @@ import org.junit.Test;
  */
 public class MinimizerSmokeTest {
 
+    private SatSolver solver = SatSolver.SAT4J;
+    private UBKind    ubKind = UBKind.INSTANCE_OR_NO_UPPER;
+
+    @Before
+    public void setSetting() {
+        Minimizer.DO_SANITY_CHECKS = false;
+    }
 
     @Test
-    public void testAllAvailableAlloyFiles() {
+    public void testHandcraftedAlloyFiles() {
+        runOnAllFiles("src/test/alloy/");
+    }
 
+    @Test
+    public void testBookAlloyFiles() {
+        runOnAllFiles("../org.alloytools.alloy.extra/extra/models/book");
+    }
+
+    @Test
+    public void testExampleAlloyFiles() {
+        runOnAllFiles("../org.alloytools.alloy.extra/extra/models/examples");
+    }
+
+    private void runOnAllFiles(String alloyFilesDir) {
         int cmdNum = 0;
         List<Path> alloyFiles = new ArrayList<>();
 
-        try (Stream<Path> stream = Files.walk(Paths.get("src/test/alloy/"))) {
+        try (Stream<Path> stream = Files.walk(Paths.get(alloyFilesDir))) {
             alloyFiles = stream.map(Path::normalize).filter(Files::isRegularFile).filter(path -> path.getFileName().toString().endsWith(".als")).collect(Collectors.toList());
         } catch (IOException e) {
             fail("Unable to list files: " + e.toString());
@@ -35,8 +58,16 @@ public class MinimizerSmokeTest {
         for (Path p : alloyFiles) {
             if (!p.toString().contains("/iterate/")) {
                 System.out.println(p);
-                Minimizer m = MinimizerUtil.testMin(p.toString(), cmdNum, UBKind.EXACT);
+                try {
+                    Minimizer m = MinimizerUtil.testMin(p.toString(), cmdNum, ubKind, solver);
+                } catch (Exception e) {
+                    if (!e.getMessage().contains("higher-order quantification")) {
+                        throw e;
+                    }
+                }
             }
         }
     }
+
+
 }
